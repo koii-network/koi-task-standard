00ce11dbe9cd86304517981659615705a19b1126
Sebhatu <abuga8@yahoo.com>
2021-06-30 16:51:51 +0200

    bug fixed

--- src/vault/index.js
+++ src/vault/index.js
@@ -3,23 +3,38 @@ export function handle(state, action) {
     const caller = action.caller;
     const owner = state.owner;
     if (input.function === "register") {
-      const nftId = input.nftId;
-      if (!nftId) throw new ContractError("nftId undefined");
-    }
-    if (owner !== caller){
-     throw new ContractError("Only the owner can register");
-    }
-    const contractState = await SmartWeave.contracts.readContractState(
-       nftId
-      );
-    if(contractState.balances[caller] !== 1){
-    throw new ContractError(`token needs to be locked first.delegate your nft to this address ${state.owner}`);
-    }
-    const nftIdOnOtherNetwork = input.nftIdOnOtherNetwork;
-    
-  
+        if (owner !== caller) {
+            throw new ContractError("Only the owner can register");
+        }
+        const nftId = input.nftId;
+        if (!nftId) throw new ContractError("nftId is undefined");
+        const contractState = await SmartWeave.contracts.readContractState(
+            nftId
+        );
 
+        if (contractState.balances[caller] !== 1) {
+            throw new ContractError(`token needs to be locked first.delegate your nft to this address ${state.owner}`);
+        }
+
+        const nftIdOnOtherNetwork = input.nftIdOnOtherNetwork;
+        if (!nftIdOnOtherNetwork) throw new ContractError("nftIdOnOtherNetwork is undefined");
+        const network = input.network;
+        if(!network) throw new ContractError("network is undefined");
+        if(network === "ethereum"){
+          const registry = state.registry;
+          registry[network][nftid] = nftIdOnOtherNetwork;
+        }else{
+        throw new ContractError("network is not supported");
+        }
+        return state;
+    }
+
+    throw new ContractError(`No function supplied or function not recognised: "${input.function}".`);
 
 
 }
+
+
+
+
 
