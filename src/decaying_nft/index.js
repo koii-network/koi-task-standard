ab61e5c59c0bab34ddd690a703c2fcba64900b4f
alexander-morris <al@openkoi.com>
2021-07-18 16:06:50 -0300

    adding example decay function

---
+++ src/decaying_nft/index.js
@@ -0,0 +1,12 @@
+import transfer from "./transfer";
+import balance from "./balance";
+import lock from "./lock";
+import unlock from "./unlock";
+
+const handlers = [transfer, balance, lock, unlock];
+
+export async function handle(state, action) {
+  const handler = handlers.find((fn) => fn.name === action.input.function);
+  if (handler) return await handler(state, action);
+  throw new ContractError(`Invalid function: "${action.input.function}"`);
+}
