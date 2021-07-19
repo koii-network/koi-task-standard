39f48c81bccaad912b24d7a21cd442f80eef2a19
Maki44 <sebhatumakda20@gmail.com>
2021-07-19 23:29:55 +0200

    test environment

--- test/attentionM.test.js
+++ test/attentionM.test.js
@@ -28,7 +28,98 @@ async function main() {
   const attentionInitState = JSON.parse(
     fs.readFileSync(`src/attentionM/init_state.json`)
   );
+
   smartest.writeContractState(attentionContractId, attentionInitState);
+  const attentionInput = {
+    function: "submitDistribution",
+    distributionTxId: "eo_YY_RAdYkWMYh7-MuBCgd3Dl-iWDIXx-vhfSZGodc",
+    cacheUrl: "http/bundler/cache",
+    mainContractId: "q5w6e7r8",
+    contractId: "a1s2d3f4"
+  };
+  await smartest.interactWrite(
+    arweave,
+    attentionSrc,
+    wallet,
+    attentionInput,
+    smartest.readContractState(attentionContractId),
+    walletAddress,
+    attentionContractId
+  );
+  const attentionInput0 = {
+    function: "submitDistribution",
+    distributionTxId: "KFyrB4SBIv5XPyRu-sBUdfuQlvDpBGQ6-q9ujVek34A",
+    cacheUrl: "http/bundler/cache",
+    mainContractId: "q5w6e7r8",
+    contractId: "a1s2d3f4"
+  };
+  await smartest.interactWrite(
+    arweave,
+    attentionSrc,
+    wallet,
+    attentionInput0,
+    smartest.readContractState(attentionContractId),
+    walletAddress,
+    attentionContractId
+  );
+  const state = smartest.readContractState(attentionContractId);
+  const proposedPaylods = state.task.proposedPaylods.find(
+    (proposedPaylod) => proposedPaylod.block === state.task.open
+  );
+  const proposedDataId = proposedPaylods.proposedDatas[0].id;
+  const attentionInput1 = {
+    function: "audit",
+    id: proposedDataId,
+    descripition: "malicious_data"
+  };
+  await smartest.interactWrite(
+    arweave,
+    attentionSrc,
+    wallet,
+    attentionInput1,
+    smartest.readContractState(attentionContractId),
+    walletAddress,
+    attentionContractId
+  );
+  const attentionInput2 = {
+    function: "vote",
+    voteId: proposedDataId,
+    userVote: true
+  };
+  await smartest.interactWrite(
+    arweave,
+    attentionSrc,
+    wallet,
+    attentionInput2,
+    smartest.readContractState(attentionContractId),
+    walletAddress,
+    attentionContractId
+  );
+  const attentionInput3 = {
+    function: "rankAndPrepareDistribution"
+  };
+  await smartest.interactWrite(
+    arweave,
+    attentionSrc,
+    wallet,
+    attentionInput3,
+    smartest.readContractState(attentionContractId),
+    walletAddress,
+    attentionContractId
+  );
+  const koiInput = {
+    function: "distributeReward"
+  };
+
+  await smartest.interactWrite(
+    arweave,
+    koiSrc,
+    wallet,
+    koiInput,
+    smartest.readContractState(koiContractId),
+    walletAddress,
+    koiContractId
+  );
 
   console.log(
     "Koi initial states: ",
