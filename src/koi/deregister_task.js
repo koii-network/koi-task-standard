a01c48208300f83382879b7283c1ae08a755b20b
Maki44 <sebhatumakda20@gmail.com>
2021-07-19 16:33:02 +0200

    update

--- src/koi/deregister_task.js
+++ src/koi/deregister_task.js
@@ -1,10 +1,10 @@
 export default function deregisterTask(state, action) {
   const caller = action.caller;
   state.koiTasks = state.tasks.filter((e) => {
-    if (e.TaskOwner !== caller) {
+    if (e.owner !== caller) {
       throw new ContractError("only owner of the task can delete a task");
     }
-    e.TaskId != action.input.taskId;
+    e.txId != action.input.taskTxId;
   });
   return { state };
 }
