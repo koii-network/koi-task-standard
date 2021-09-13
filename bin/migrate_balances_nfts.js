82cdeef1efc12a19be94c42d9e9265e9c9dd4a89
Andre Vallestero <andrevallestero@gmail.com>
2021-09-12 20:06:26 -0400

    Update migrate script

--- bin/migrate_balances_nfts.js
+++ bin/migrate_balances_nfts.js
@@ -11,6 +11,8 @@ const arweave = Arweave.init({
   logging: false
 });
 
+const LEGIT_NFT = "Vh-o7iOqOYOOHhUW2z9prtrtH8hymQyjX-rTxAY0jjU";
+
 async function main() {
   // console.log("Computing old state");
   // const oldKoiiState = await smartweave.readContract(
@@ -18,21 +20,59 @@ async function main() {
   //   "cETTyJQYxJLVQ6nC3VxzsZf1x2-6TW2LFkGZa91gUWc"
   // );
 
+  console.log("getting state");
   const oldKoiiState = (
-    await axios.get("https://bundler.openkoi.com:8888/state/current")
+    await axios.get("https://devbundler.openkoi.com:8888/state/current")
   ).data;
-
   const oldNfts = oldKoiiState.registeredRecord;
-  const nftMap = {};
 
+  // Get spam owners
+  console.log("getting top content");
+  const topContent = (
+    await axios.get(
+      "https://devbundler.openkoi.com:8888/state/top-content-predicted"
+    )
+  ).data;
+  const spamOwners = new Set();
+  for (let i = 0; i < topContent.length; ++i) {
+    const content = Object.values(topContent[i])[0];
+    if (content.txIdContent === LEGIT_NFT) break;
+    spamOwners.add(content.owner);
+  }
+  const spamNfts = [];
+
+  // Migrate nfts and remove spammer nfts
+  const newNfts = {};
   for (const nftId in oldNfts) {
     const owner = oldNfts[nftId];
-    if (owner in nftMap) nftMap[owner].push(nftId);
-    else nftMap[owner] = [nftId];
+    if (spamOwners.has(owner)) {
+      spamNfts.push(nftId);
+      continue;
+    }
+    if (owner in newNfts) newNfts[owner].push(nftId);
+    else newNfts[owner] = [nftId];
   }
 
-  fs.writeFileSync("dist/nft_map.json", JSON.stringify(nftMap));
+  // Migrate and remove spammer balance
+  for (const owner in oldKoiiState.balances)
+    if (spamOwners.has(owner)) delete oldKoiiState.balances[owner];
+
+  // Migrate attention and remove spammer attention
+  const oldRewardReport = oldKoiiState.stateUpdate.trafficLogs.rewardReport;
+  const newAttentionReport = [];
+  for (const report of oldRewardReport) {
+    const logsSummary = report.logsSummary;
+    for (const spamNft of spamNfts)
+      if (spamNft in logsSummary) {
+        delete logsSummary[spamNft];
+      }
+    newAttentionReport.push(logsSummary);
+  }
+
+  // Write to files in dist to be manually moved into init_state.json
+  fs.writeFileSync("dist/nft_map.json", JSON.stringify(newNfts));
   fs.writeFileSync("dist/balances.json", JSON.stringify(oldKoiiState.balances));
+  fs.writeFileSync("dist/attention.json", JSON.stringify(newAttentionReport));
 }
 
 main();
