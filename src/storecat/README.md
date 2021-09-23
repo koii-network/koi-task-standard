577330668410374ff631043ad9d825d8bbc774d5
inovatedTeam <bluedreampro2017@gmail.com>
2021-09-22 20:26:41 -0400

    first storecat

---
+++ src/storecat/README.md
@@ -0,0 +1,43 @@
+<h1 align="center">Template Task</h1>
+<p align="center">
+ <img align="center" height=512px src="diagram/koi_task_diagram.jpg?raw=true"></a>
+</p>
+
+This folder contains a template of the main features of a Koii Task. See the full docs in the parent folder for more information.
+
+To register a new task, use the process below.
+
+## RegisterExecutable
+
+- takes `executableId` as input.
+
+- Only owner of the contract can register the executable id.
+
+```bash
+export default function registerExecutableId(state, action) {
+  const input = action.input;
+  const caller = action.caller;
+  const executableId = input.executableId;
+  const owner = state.owner;
+  if (caller !== owner) {
+    throw new ContractError("Only owner can register");
+  }
+  if (!executableId) throw new ContractError("Invalid input");
+  if (typeof executableId !== "string")
+    throw new ContractError("Invalid input format");
+  if (executableId.length !== 43)
+    throw new ContractError("Input should have 43 characters");
+
+  state.executableId = executableId;
+  return { state };
+}
+
+
+
+```
+
+## Testing Contract
+
+`yarn build`
+
+- `node test/attention.test.js path/to/wallet.json`
---
+++ src/storecat/executable.js
@@ -0,0 +1,120 @@
+/*
+Available APIs:
+
+tools
+require
+namespace {
+  redisGet()
+  redisSet()
+  fs()
+  express()
+}
+*/
+
+// Import SDK modules if you want to use them (optional)
+const Arweave = require("arweave");
+const kohaku = require("@_koi/kohaku");
+const axios = require("axios");
+const crypto = require("crypto");
+
+const arweave = Arweave.init({
+  host: "arweave.net",
+  protocol: "https",
+  port: 443,
+  timeout: 60000,
+  logging: false
+});
+
+// Define system constants
+const FOO = "BAR";
+
+// You can also access and store files locally
+const logsInfo = {
+  filename: "history.log"
+};
+
+// Define the setup block - node, external endpoints must be registered here using the namespace.express toolkit
+function setup(_init_state) {
+  if (namespace.app) {
+    namespace.express("get", "/", someMethod);
+  }
+}
+
+// Define the execution block (this will be triggered after setup is complete)
+async function execute(_init_state) {
+  let state, block;
+  for (;;) {
+    await rateLimit();
+    try {
+      [state, block] = await getAttentionStateAndBlock();
+    } catch (e) {
+      console.error("Error while fetching attention state and block", e);
+      continue;
+    }
+    try {
+      await (namespace.app ? service : witness)(state, block);
+    } catch (e) {
+      console.error("Error while performing attention task:", e);
+    }
+  }
+}
+
+async function someMethod(_req, res) {
+  res
+    .status(200)
+    .type("application/json")
+    .send("Hello world");
+}
+
+/*
+  An audit contract can optionally be implemented when using gradual consensus (see https://koii.network/gradual-consensus.pdf for more info)
+*/
+async function audit(state) {
+  const task = state.task;
+  const activeProposedData = task.proposedPayloads.find(
+    (proposedData) => proposedData.block === task.open
+  );
+  const address = tools.address;
+  const proposedData = activeProposedData.proposedData;
+  await Promise.allSettled(
+    proposedData.map(async (proposedData) => {
+      if (proposedData.distributer !== address) {
+        const valid = await auditPort(proposedData.txId, proposedData.cacheUrl);
+        if (!valid) {
+          const input = {
+            function: "audit",
+            id: proposedData.txId
+          };
+          const task = "submit audit";
+          const tx = await kohaku.interactWrite(
+            arweave,
+            tools.wallet,
+            namespace.taskTxId,
+            input
+          );
+
+          if (await checkTxConfirmation(tx, task))
+            console.log("audit submitted");
+        }
+      }
+    })
+  );
+  hasAudited = true;
+}
+
+function canAudit(state, block) {
+  const task = state.task;
+  if (block >= task.close) return false;
+
+  const activeProposedData = task.proposedPayloads.find(
+    (proposedData) => proposedData.block === task.open
+  );
+
+  const proposedData = activeProposedData.proposedData;
+
+  return (
+    block < task.open + OFFSET_BATCH_VOTE_SUBMIT && // block in time frame
+    !hasAudited && // ports not submitted
+    proposedData.length !== 0
+  );
+}
