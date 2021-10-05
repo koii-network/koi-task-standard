8174c088478a68ef374fdf43ef0e3a8a4c81ba8e
Maki44 <sebhatumakda20@gmail.com>
2021-10-05 16:50:01 +0200

    add extra check in syncOwnership and migrate

--- bin/interact.js
+++ bin/interact.js
@@ -36,17 +36,23 @@ const contractId = "T7NmmpxLSZsWrjl2-A1KgEuOi9kXqb8FAb4tZAjeTm0"; // test koii C
 // const input = {
 //   function: "rankPrepDistribution"
 // };
-const input = {
-  function: "registerTask",
-  taskName: "Attention_Game",
-  taskTxId: "RbHbau1GLamtzyB-Wg3XlJJRGPJaO2hl-xjswFydd1w"
-};
 // const input = {
-//   function: "burnKoi",
-//   contractId: "COrDoKhQwlplYWgShhTF4YpFtFQRFJ1gunZGCx36Xa0",
-//   contentType: "nft",
-//   contentTxId: "hAvOySHDaBo9KE4m3cvDhlC4yUh_VjR6rhUIS2c47bs"
+//   function: "registerTask",
+//   taskName: "Attention_Game",
+//   taskTxId: "RbHbau1GLamtzyB-Wg3XlJJRGPJaO2hl-xjswFydd1w"
 // };
+// "EKW3AApL4mdLc6sIhVr3Cn8VN7N9VAQUp2BNALHXFtQ": {
+//     "sQTWslyCdKF6oeQ7xXUYUV1bluP0_5-483FXH_RVZKU": 1
+//   },
+//   "1UDe0Wqh51-O03efPzoc_HhsUPrmgBR2ziUfaI7CpZk": {
+//     "WL32qc-jsTxCe8m8RRQfS3b3MacsTQySDmJklvtkGFc": 1
+//   },
+const input = {
+  function: "burnKoi",
+  contractId: "OExsbvsnl5qy5sj6pa5_zyUg_4IwU8iQgfTkK_vTZy4",
+  contentType: "nft",
+  contentTxId: "eBwSsqdX4rASxCpEeUylrD4K4hZb8D1u6FefKvZyChk"
+};
 // const input = {
 //   function: "mint",
 //   target: "6VJYLb6lvBISrgRbhd1ODHzJ1xAh3ZA3OdSY20E88Bg",
