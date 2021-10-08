58b5c70971542172e38738616fbc6448c6a7d776
Eagle930705 <eagle930705@gmail.com>
2021-10-08 15:30:15 -0400

    test a func on scraper

--- src/storecat/scraper.js
+++ src/storecat/scraper.js
@@ -1,1 +1,11 @@
 const cheerio = require('cheerio');
+
+const parseAndSaveAllSelctor = async ($, selector, contentType) => {
+  console.log($);
+  console.log(selector);
+  console.log(contentType);
+}
+
+export {
+  parseAndSaveAllSelctor
+}
