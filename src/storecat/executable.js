d9c9e520fbf2fe61bf53b8b7d7a4a15a89263edb
inovatedTeam <bluedreampro2017@gmail.com>
2021-10-08 23:17:59 -0400

    update audit

--- src/storecat/executable.js
+++ src/storecat/executable.js
@@ -123,26 +123,44 @@ async function witness(state, block) {
 }
 
 /*
+  canAudit : is it possible to audit
+  return : boolean
+*/
+function canAudit(state, block) {
+  const task = state.task;
+  if (block >= task.close) return false;
+  if (block < task.open + OFFSET_PER_DAY) {
+    // string scraping require
+    return false;
+  }
+  if (hasAudited) return false;
+
+  if (state.payloads && state.payloads.length > 0) {
+    return true;
+  }
+}
+/*
   An audit contract can optionally be implemented when using gradual consensus (see https://koii.network/gradual-consensus.pdf for more info)
 */
 async function audit(state) {
   const task = state.task;
   const address = tools.address;
   // check payload ranking
-  hasAudited = true;
-}
-function canAudit(state, block) {
-  const task = state.task;
-  if (block >= task.close) return false;
-  if (block < task.open + OFFSET_PER_DAY){
-    // string scraping require
-    return false;
-  }
-  if (hasAudited) return false;
+  // const input = {
+  //   function: "audit",
+  //   id: proposedData.txId
+  // };
+  // const task = "submit audit";
+  // const tx = await kohaku.interactWrite(
+  //   arweave,
+  //   tools.wallet,
+  //   namespace.taskTxId,
+  //   input
+  // );
 
-  if (state.payloads && state.payloads.length > 0){
-    return true;
-  }
+  // if (await checkTxConfirmation(tx, task))
+  //   console.log("audit submitted");
+  hasAudited = true;
 }
 
 async function writePayloadInPermaweb() {
