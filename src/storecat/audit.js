843bc0400c1ae15a5dd61db69508626329528533
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-10-13 13:22:52 -0700

    updated audit

--- src/storecat/audit.js
+++ src/storecat/audit.js
@@ -1,15 +1,16 @@
 // The Audit function can be included optionall as a way of invoking a stake slashing behavior to penalize bad actors
+async function getWinner() {
 
-export default function audit(state, action) {
-  const votes = state.votes;
+}
+
+export default async function audit(state, action) {
+  const tasks = state.tasks;
   const caller = action.caller;
   const input = action.input;
-  const id = input.id;
 
-  if (!id) throw new ContractError("Invalid input");
-  if (typeof id !== "string") throw new ContractError("Invalid input format");
-  if (id.length !== 43) {
-    throw new ContractError("Input should have 43 characters");
+  if(tasks.length == 0) throw new ContractError("There is no tasks to audit");
+  for(let i = 0; i < tasks.length; i++) {
+    // async function
   }
   if (SmartWeave.block.height > state.task.open + 600) {
     throw new ContractError("audit is closed. wait for another round");
@@ -18,15 +19,5 @@ export default function audit(state, action) {
   if (triggeredVote !== undefined) {
     throw new ContractError(`Vote is triggered with ${id} id`);
   }
-  const vote = {
-    id: id,
-    status: "active",
-    voteTrigger: caller,
-    yays: 0,
-    nays: 0,
-    votersList: [],
-    bundlers: {}
-  };
-  votes.push(vote);
   return { state };
 }
