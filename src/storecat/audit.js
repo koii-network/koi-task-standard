3bdbe30aea43a7ed8c91b10f56da8c6962db0abb
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-10-18 20:49:34 -0700

    update distribution

--- src/storecat/audit.js
+++ src/storecat/audit.js
@@ -67,8 +67,10 @@ export default async function audit(state, action) {
       // set bounty process
       // 1 discount bounty from requester
       // -- if the owner of scraper didn't enough bounty balance, this scraper will be ignored
+      task.prepareDistribution.push({task.owner: task.bounty * -1})
       // 2 set bounty to winner - top 8 nodes
       let deeper = 0;
+
       task.payloads.forEach((hash) => {
         if (hash.hashPayload == topHash && deeper < 8) {
           deeper++;
@@ -76,6 +78,7 @@ export default async function audit(state, action) {
           let qty = Number(task.bounty * Math.pow(2, deeper * -1));
           // if (balances[hash.owner]) balances[hash.owner] += qty;
           // else balances[hash.owner] = qty;
+          task.prepareDistribution.push({hash.owner: qty})
           console.log(
             "set bounty target - " +
               hash.owner +
