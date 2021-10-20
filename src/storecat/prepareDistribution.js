cc3da4ce067cf0d3ed5439912bb6261cf4cf397e
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-10-19 19:05:35 -0700

    updated prepareDistribution

--- src/storecat/prepareDistribution.js
+++ src/storecat/prepareDistribution.js
@@ -1,31 +1,20 @@
 export default async function prepareDistribution(state) {
   const tasks = state.tasks;
 
-  let task = {};
-  let block = SmartWeave.block.height;
-
+  let matchIndex = -1;
   for (let index = 0; index < tasks.length; index++) {
     const element = tasks[index];
-    if (block >= task.close && task.hasAudit) {
-      task = element;
+    if (block >= element.close && !element.hasAudit && element.payloads.length > 0) {
+      matchIndex = index;
       break;
     }
   }
-
-  if(task.open !== undefined) {
-    const prepareDistribution = task.prepareDistribution;
-
+  if(matchIndex === -1)
+    throw new ContractError("There is no task to audit");
   }
-  // tasks.some((t, index) => {
-  //   if (block >= task.close && task.hasAudit) {
-  //     findIndex = index;
-  //     return true;
-  //   }
-  // })
-  // if(findIndex > -1)
-  //   task = state.tasks[findIndex];
-  // else {
-  //   throw new ContractError("There is no task to audit");
-  // }
+  const task = tasks[matchIndex];
+  if(task.hasOwnProperty('open')) {
+    // calc top hash and prepare balance
+  }
   return { state };
 }
