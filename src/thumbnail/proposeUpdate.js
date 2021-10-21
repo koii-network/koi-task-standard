a2b73515b73a40024f8b74188414fc29b6a5979e
Somali0128 <ldydq970128@gmail.com>
2021-10-20 23:30:52 -0300

    update contract

--- src/thumbnail/proposeUpdate.js
+++ src/thumbnail/proposeUpdate.js
@@ -1,6 +1,7 @@
 // The Audit function can be included optionall as a way of invoking a stake slashing behavior to penalize bad actors
 
 export default function proposeUpdate(state, action) {
+    const votes = state.votes;
     const caller = action.caller; // the arweave ID who is calling the function
     const input = action.input; // the input action value (data payload, aid, cid)
     const aid = input.aid;
@@ -19,9 +20,24 @@ export default function proposeUpdate(state, action) {
       throw new ContractError("cid should have 46 characters");
     }
 
-    // if it passes all the tests, update the state
-    state.thumbnails[aid] = cid;
-    
+    if (SmartWeave.block.height > state.task.open + 600) {
+      throw new ContractError("thumbnail is closed. wait for another round");
+    }
+    const triggeredVote = votes.find((vote) => vote.id == id);
+    if (triggeredVote !== undefined) {
+      throw new ContractError(`Vote is triggered with ${id} id`);
+    }
+    const vote = {
+      aid: aid,
+      cud: cid,
+      status: "active",
+      voteTrigger: caller,
+      yays: 0,
+      nays: 0,
+      votersList: [],
+      bundlers: {}
+    };
+    votes.push(vote);
     return { state };
   }
   
