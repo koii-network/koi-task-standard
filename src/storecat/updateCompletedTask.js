b240930d7f062bc801416814e3e758d340c50497
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-10-26 20:04:15 -0700

    updated compelted Task

--- src/storecat/updateCompletedTask.js
+++ src/storecat/updateCompletedTask.js
@@ -3,10 +3,19 @@ export default async function addScrapingRequest(state, action) {
   const input = action.input;
   const matchIndex = input.matchIndex;
   
+  if(typeof matchIndex !== 'number') {
+    throw new ContractError("Task index should be a number");
+  }
+  
   // call interactWrite func update task
   const task = tasks[matchIndex];
   // update completed task 
-  
+  const completedTask = {
+    uuid: task.uuid,
+    owner: task.owner,
+    tdId: task.tId
+  };
+  task.completedTasks.push(completedTask);
   // remove task body
   tasks.push(task);
   return { state };
