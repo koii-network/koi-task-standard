f16371636694761adf19920897c358f147d75bd2
Eagle930705 <eagle930705@gmail.com>
2021-10-28 10:50:14 -0400

    completed code prettify

--- src/storecat/savePayload.js
+++ src/storecat/savePayload.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
 export default async function savePayload(state, action) {
   const tasks = state.tasks;
   const input = action.input;
   const matchIndex = input.matchIndex;
   const payload = input.payload;
-  
-  if(typeof matchIndex !== 'number') {
+
+  if (typeof matchIndex !== "number") {
     throw new ContractError("Task index should be a number");
   }
 
-  if(typeof payload !== 'object') {
+  if (typeof payload !== "object") {
     throw new ContractError("Payload should be an object");
   }
 
@@ -16,17 +17,17 @@ export default async function savePayload(state, action) {
     hashPayload: payload.hashPayload,
     owner: payload.owner
   });
-  
+
   let isFounded = false;
   for (let index = 0; index < tasks[matchIndex].payloadHashs.length; index++) {
     const element = tasks[matchIndex].payloadHashs[index];
-    if(element.hashPayload === payload.hashPayload) {
+    if (element.hashPayload === payload.hashPayload) {
       isFounded = true;
       tasks[matchIndex].payloadHashs[index].count++;
     }
   }
 
-  if(!isFounded) {
+  if (!isFounded) {
     tasks[matchIndex].payloadHashs.push({
       payload: payload.payload,
       hashPayload: payload.hashPayload,
