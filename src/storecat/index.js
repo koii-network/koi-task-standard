b9fdbfc42493418a11aa2e61070f2f6b97758d1b
Eagle930705 <eagle930705@gmail.com>
2021-11-01 13:44:56 -0400

    removed import in index.js

--- src/storecat/index.js
+++ src/storecat/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-undef */
 // The index.js file handles compiling the pieces of the contract into a single Smartweave contract file.
-import audit from "./audit";
-import addScrapingRequest from "./addScrapingRequest";
-import savePayload from "./savePayload";
-import confirmDistributeReward from "./confirmDistributeReward";
-import savedPayloadToPermaweb from "./savedPayloadToPermaweb";
-import updateCompletedTask from "./updateCompletedTask";
+// import audit from "./audit";
+// import addScrapingRequest from "./addScrapingRequest";
+// import savePayload from "./savePayload";
+// import confirmDistributeReward from "./confirmDistributeReward";
+// import savedPayloadToPermaweb from "./savedPayloadToPermaweb";
+// import updateCompletedTask from "./updateCompletedTask";
+
+const audit = require("./audit");
+const addScrapingRequest = require("./addScrapingRequest");
+const savePayload = require("./savePayload");
+const confirmDistributeReward = require("./confirmDistributeReward");
+const savedPayloadToPermaweb = require("./savedPayloadToPermaweb");
+const updateCompletedTask = require("./updateCompletedTask");
 
 const handlers = [
   audit,
