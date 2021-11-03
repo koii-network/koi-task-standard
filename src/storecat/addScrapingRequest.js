15360f563add0763d5ff68e6c77bf58982960059
Eagle930705 <eagle930705@gmail.com>
2021-11-02 21:15:16 -0400

    temp for testing

--- src/storecat/addScrapingRequest.js
+++ src/storecat/addScrapingRequest.js
@@ -3,21 +3,21 @@ export default async function addScrapingRequest(state, action) {
   const tasks = state.tasks;
   const input = action.input;
   const scrapingRequest = input.scrapingRequest;
-  const koiiContract = state.koiiContract;
-  const koiiState = await SmartWeave.contracts.readContractState(koiiContract);
-  const balances = koiiState.balances;
+  // const koiiContract = state.koiiContract;
+  // const koiiState = await SmartWeave.contracts.readContractState(koiiContract);
+  // const balances = koiiState.balances;
 
-  if (!(scrapingRequest.owner in balances)) {
-    throw new ContractError(
-      "Scraping owner should have minimum stake of " +
-        scrapingRequest.bounty +
-        " koii"
-    );
-  }
-  const balanceOwner = balances[scrapingRequest.owner];
-  if (balanceOwner <= Number(scrapingRequest.bounty)) {
-    throw new ContractError("Owner koii balance is not enough");
-  }
+  // if (!(scrapingRequest.owner in balances)) {
+  //   throw new ContractError(
+  //     "Scraping owner should have minimum stake of " +
+  //       scrapingRequest.bounty +
+  //       " koii"
+  //   );
+  // }
+  // const balanceOwner = balances[scrapingRequest.owner];
+  // if (balanceOwner <= Number(scrapingRequest.bounty)) {
+  //   throw new ContractError("Owner koii balance is not enough");
+  // }
 
   // call interactWrite func update task
   let task = {
