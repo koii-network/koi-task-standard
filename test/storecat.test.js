9cddf932da2c531247254497ed8ca02982c46690
Eagle930705 <eagle930705@gmail.com>
2021-11-03 12:31:14 -0400

    update testing logic

--- test/storecat.test.js
+++ test/storecat.test.js
@@ -1,58 +1,61 @@
 const smartest = require("@_koi/smartest");
 const Arweave = require("arweave");
 const fs = require("fs");
-// const axios = require("axios").default;
-// const ClusterUtil = require("../src/storecat/cluster");
-// const ScraperUtil = require("../src/storecat/scraper");
+const axios = require("axios").default;
+const ClusterUtil = require("../src/storecat/cluster");
+const ScraperUtil = require("../src/storecat/scraper");
 
 if (process.argv[2] === undefined) throw "Wallet path not defined";
 
-// async function getPayload(url) {
-//   try {
-//     let cluster = await ClusterUtil.puppeteerCluster();
-//     const { html } = await cluster.execute({
-//       url,
-//       takeScreenshot: false
-//     });
-//     const scrapingData = await ScraperUtil.getPayload(html);
-//     console.log(
-//       "**************** finished scraping *******************",
-//       scrapingData
-//     );
-//     return scrapingData;
-//   } catch (error) {
-//     console.log("get payload error", error);
-//     return false;
-//   }
-// }
+const arweave = Arweave.init({
+  host: "arweave.net",
+  port: 443,
+  protocol: "https",
+  timeout: 20000,
+  logging: false
+});
 
+const wallet = JSON.parse(fs.readFileSync(process.argv[2]));
+
+
+// Load koi contract
+const koiSrc = fs.readFileSync(`dist/koii-core.js`, "utf8");
+const koiContractId = "a1s2d3f4";
+const koiInitState = JSON.parse(
+  fs.readFileSync(`src/koii-core/init_state.json`)
+);
+smartest.writeContractState(koiContractId, koiInitState);
+
+// Load storecat contract
+const storecatSrc = fs.readFileSync(`dist/storecat.js`, "utf8");
+const storecatContractId = "q2w3e5r7";
+const storecatInitState = JSON.parse(
+  fs.readFileSync(`src/storecat/init_state_test.json`)
+);
+smartest.writeContractState(storecatContractId, storecatInitState);
+async function getPayload(url) {
+  try {
+    let cluster = await ClusterUtil.puppeteerCluster();
+    const { html } = await cluster.execute({
+      url,
+      takeScreenshot: false
+    });
+    const scrapingData = await ScraperUtil.getPayload(html);
+    console.log(
+      "**************** finished scraping *******************",
+      scrapingData
+    );
+    return scrapingData;
+  } catch (error) {
+    console.log("get payload error", error);
+    return false;
+  }
+}
+async function test_upload_payload_to_arweave() {
+
+}
 async function main() {
-  const arweave = Arweave.init({
-    host: "arweave.net",
-    port: 443,
-    protocol: "https",
-    timeout: 20000,
-    logging: false
-  });
-
-  const wallet = JSON.parse(fs.readFileSync(process.argv[2]));
   const walletAddress = await arweave.wallets.jwkToAddress(wallet);
-
-  // Load koi contract
-  const koiSrc = fs.readFileSync(`dist/koii-core.js`, "utf8");
-  const koiContractId = "a1s2d3f4";
-  const koiInitState = JSON.parse(
-    fs.readFileSync(`src/koii-core/init_state.json`)
-  );
-  smartest.writeContractState(koiContractId, koiInitState);
-
-  // Load storecat contract
-  const storecatSrc = fs.readFileSync(`dist/storecat.js`, "utf8");
-  const storecatContractId = "q2w3e5r7";
-  const storecatInitState = JSON.parse(
-    fs.readFileSync(`src/storecat/init_state_test.json`)
-  );
-  smartest.writeContractState(storecatContractId, storecatInitState);
 
   // --- test get scraping request
   // let url = "https://app.getstorecat.com:8888/api/v1/bounty/getScrapingUrl";
