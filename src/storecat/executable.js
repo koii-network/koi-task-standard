23230ffb4c53a299e08869cc20f0022c7680b454
Eagle930705 <eagle930705@gmail.com>
2021-11-09 20:21:33 -0500

    added parseAndSaveAllSelctor

--- src/storecat/executable.js
+++ src/storecat/executable.js
@@ -498,3 +498,87 @@ async function getPayloadFromHtml(html) {
   payload.content = await getScrapData($("body").html());
   return payload;
 }
+
+async function parseAndSaveAllSelctor($, selector, contentType) {
+  let totalContent = [];
+  $(selector).each((index, element) => {
+    if (index < 500) {
+      // add limitation 500 nodes
+      let content = "";
+      if (selector === "img") {
+        content = $(element).attr("src");
+      } else if (selector === "a") {
+        content = $(element).attr("href");
+      } else {
+        content = $(element).text().trim();
+      }
+      let ids = $(element).attr("id");
+      let classNames = $(element).attr("class");
+      let subSelectorsArr = [];
+      let subSelector = "";
+      try {
+        while ($(element)[0] && $(element)[0].name && !ids && !classNames) {
+          subSelectorsArr.push($(element)[0].name);
+          element = $(element).parent();
+          ids = $(element).attr("id");
+          classNames = $(element).attr("class");
+        }
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      for (let i = subSelectorsArr.length - 1; i >= 0; i--) {
+        subSelector += `>${subSelectorsArr[i]}`;
+      }
+      if ($(element)[0] && $(element)[0].name) {
+        const elementType = $(element)[0].name;
+        let modifiedClassNames = ""; //contains .
+        let modifiedIds = ""; //contains #
+
+        if (classNames) {
+          modifiedClassNames = `[class='${classNames}']`;
+        }
+        if (ids) {
+          modifiedIds = `[id='${ids}']`;
+        }
+        let finalSelector = `${elementType}`;
+        if (modifiedClassNames) finalSelector += `${modifiedClassNames}`;
+        if (modifiedIds) finalSelector += `${modifiedIds}`;
+        finalSelector += subSelector;
+        let found = false;
+        if ($(finalSelector).length >= 0) {
+          $(finalSelector).each(async (i, item) => {
+            if (selector === "img") {
+              content = $(item).attr("src");
+            } else if (selector === "a") {
+              content = $(item).attr("href");
+            } else {
+              content = $(item).text().trim();
+            }
+            if (!found) {
+              // finalSelectorIndex = i;
+              let finalSelectorWithIndex = `${i}$` + finalSelector;
+              if (
+                content &&
+                content.trim() !== "" &&
+                content[0] !== "#" &&
+                totalContent.filter((x) => x.text === content).length === 0
+              ) {
+                totalContent.push({
+                  text: content,
+                  type: contentType,
+                  selector: finalSelectorWithIndex,
+                  label: "sitemap " + contentType
+                });
+                found = true;
+              }
+            }
+          });
+        }
+      } else {
+        return;
+      }
+    }
+  });
+  return totalContent;
+}
